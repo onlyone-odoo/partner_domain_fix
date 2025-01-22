48337ca1fc78fe042b4fffb1caa079cab728c83f
onlyone-odoo <matiasb@onlyone.com.ar>
2025-01-22 12:20:09 -0300

    Initial commit: Complete module partner_domain_fix

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Domain fix",
+    "summary": """
+        Edit partner_id domains in sale orders and other views to select only "non-child of" partners as clients """,
+    "author": "Be OnlyOne",
+    "maintainers": ["onlyone-odoo"],
+    "website": "https://onlyone.odoo.com/",
+    "license": "AGPL-3",
+    "category": "Technical Settings",
+    "version": "17.0.1.0.0",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": ["base"],
+    "data": [
+        "views/sale_order_view.xml",
+    ],
+}
